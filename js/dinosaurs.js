fba9d6047452bf2fce288da34645c7961bff8989
A23I005 <A23I005hn@ous.jp>
2026-06-12 10:46:03 +0900

    こりとさうるすの大きさの変更

--- js/dinosaurs.js
+++ js/dinosaurs.js
@@ -50,7 +50,7 @@ window.DINOSAURS = [
     collectionDistance: 3.1,
     collectionYOffset: 0.04,
     collectionRotation: '0 0 0',
-    arFitTarget: 1.25,  // ⭕ AR画面での3Dモデルの大きさ
+    arFitTarget: 2.25,  // ⭕ AR画面での3Dモデルの大きさ
     arYOffset: 0.05     // ⭕ AR画面での上下位置
   },
   {
